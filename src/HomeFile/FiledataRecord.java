5fb31f6122d959166d71e9f02411d9cf35b1237a
Nanaika <zloyx2@mail.ru>
2020-10-02 14:01:57 +0500

    sssssss

--- src/HomeFile/FiledataRecord.java
+++ src/HomeFile/FiledataRecord.java
@@ -13,7 +13,7 @@ public class FiledataRecord {
 
         try {
             try {
-                FileWriter fw = new FileWriter("logins.txt");
+                FileWriter fw = new FileWriter("logins5555.txt");
 
 
                 for (int i = 0; i < arrayLogin.length; i++) {
